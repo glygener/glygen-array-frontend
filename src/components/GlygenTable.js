841bdd08f75c48b15d23e4c357b6a232da74d21e
Tatiana <tbw32@hotmail.com>
2021-09-27 11:00:01 -0400

    GlyGen table  NoDataComponent

--- src/components/GlygenTable.js
+++ src/components/GlygenTable.js
@@ -13,7 +13,7 @@ import mirageIcon from "../images/mirageIcon.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CardLoader from "./CardLoader";
 
-const GlygenTable = props => {
+const GlygenTable = (props) => {
   const history = useHistory();
   const [rows, setRows] = useState(0);
   const [data, setData] = useState([]);
@@ -45,7 +45,7 @@ const GlygenTable = props => {
     setShowDeleteModal(true);
   };
 
-  const makePublicPrompt = id => {
+  const makePublicPrompt = (id) => {
     setSelectedIdMakePublic(id);
     setShowMakePublicModal(true);
   };
@@ -56,7 +56,9 @@ const GlygenTable = props => {
     wsCall(
       props.deleteWS,
       "DELETE",
-      queryParamId ? { qsParams: { datasetId: queryParamId }, urlParams: [selectedId] } : [selectedId],
+      queryParamId
+        ? { qsParams: { datasetId: queryParamId }, urlParams: [selectedId] }
+        : [selectedId],
       true,
       null,
       deleteSuccess,
@@ -64,7 +66,7 @@ const GlygenTable = props => {
     );
   };
 
-  const handleFilterChange = e => {
+  const handleFilterChange = (e) => {
     setSearchFilter(e.target.value);
   };
 
@@ -73,7 +75,7 @@ const GlygenTable = props => {
       Header: "Comments",
       accessor: props.commentsRefColumn,
       style: {
-        textAlign: "center"
+        textAlign: "center",
       },
       // eslint-disable-next-line react/display-name
       Cell: (row, index) =>
@@ -111,7 +113,7 @@ const GlygenTable = props => {
         ) : (
           <div key={index}></div>
         ),
-      minWidth: 80
+      minWidth: 80,
     };
   }
 
@@ -119,7 +121,7 @@ const GlygenTable = props => {
     columnsToRender["actionsColumn"] = {
       Header: "Actions",
       style: {
-        textAlign: "center"
+        textAlign: "center",
       },
       sortable: false,
       // eslint-disable-next-line react/display-name
@@ -150,7 +152,9 @@ const GlygenTable = props => {
                 size="xs"
                 title="View"
                 className="table-btn  edit-icon"
-                onClick={() => history.push("/" + props.viewUrl + "/" + row.original[props.keyColumn])}
+                onClick={() =>
+                  history.push("/" + props.viewUrl + "/" + row.original[props.keyColumn])
+                }
               />
             </>
           )}
@@ -163,7 +167,9 @@ const GlygenTable = props => {
                 size="xs"
                 title="Edit"
                 className="table-btn edit-icon"
-                onClick={() => history.push("/" + props.editUrl + "/" + row.original[props.keyColumn])}
+                onClick={() =>
+                  history.push("/" + props.editUrl + "/" + row.original[props.keyColumn])
+                }
               />
             </>
           )}
@@ -177,7 +183,9 @@ const GlygenTable = props => {
                 title="Clone"
                 className="table-btn"
                 onClick={() =>
-                  history.push("/" + props.copyUrl + "/" + row.original[props.keyColumn] + "?" + props.copyPage)
+                  history.push(
+                    "/" + props.copyUrl + "/" + row.original[props.keyColumn] + "?" + props.copyPage
+                  )
                 }
               />
             </>
@@ -218,7 +226,7 @@ const GlygenTable = props => {
           )}
         </>
       ),
-      minWidth: 100
+      minWidth: 100,
     };
   }
 
@@ -233,7 +241,7 @@ const GlygenTable = props => {
           onClick={() => props.selectButtonHandler(row.original, props.isModal)}
           value={props.selectButtonHeader || "Select"}
         />
-      )
+      ),
     };
   }
 
@@ -248,7 +256,7 @@ const GlygenTable = props => {
           onChange={props.checkboxChangeHandler.bind(this, row.original)}
           defaultChecked={props.defaultCheckboxHandler(row.original)}
         />
-      )
+      ),
     };
   }
 
@@ -264,7 +272,7 @@ const GlygenTable = props => {
           onChange={() => props.selectRadioHandler(row.original)}
           checked={row.original.name === selectedRadio ? true : false}
         />
-      )
+      ),
     };
   }
 
@@ -297,7 +305,7 @@ const GlygenTable = props => {
               <Col
                 style={{
                   marginTop: "10px",
-                  marginLeft: "40px"
+                  marginLeft: "40px",
                 }}
               >
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -315,7 +323,7 @@ const GlygenTable = props => {
                 md={{ span: 3, offset: props.showOnlyMyLinkersOrGlycansCheckBox ? 0 : 2 }}
                 style={{
                   marginBottom: "10px",
-                  marginRight: props.showOnlyMyLinkersOrGlycansCheckBox ? "-50px" : ""
+                  marginRight: props.showOnlyMyLinkersOrGlycansCheckBox ? "-50px" : "",
                 }}
               >
                 <Form.Control
@@ -351,8 +359,8 @@ const GlygenTable = props => {
         multiSort={false}
         showPaginationTop
         manual
-        ref={element => setTableElement(element)}
-        onFetchData={state => {
+        ref={(element) => setTableElement(element)}
+        onFetchData={(state) => {
           /*state obj structure:
           {
             sorted: [
@@ -368,7 +376,12 @@ const GlygenTable = props => {
           if (props.fetchWS) {
             setShowLoading(true);
             var sortColumn = state.sorted.length > 0 ? state.sorted[0].id : props.defaultSortColumn;
-            var sortOrder = state.sorted.length > 0 ? (state.sorted[0].desc === false ? 1 : 0) : props.defaultSortOrder;
+            var sortOrder =
+              state.sorted.length > 0
+                ? state.sorted[0].desc === false
+                  ? 1
+                  : 0
+                : props.defaultSortOrder;
             wsCall(
               props.fetchWS,
               "GET",
@@ -382,8 +395,8 @@ const GlygenTable = props => {
                   loadAll: false, //only useful for features, blocks and slides
                   filter: searchFilter !== "" ? searchFilter : "",
                   type: props.paramTypeValue,
-                  ...props.qsParams
-                }
+                  ...props.qsParams,
+                },
               },
               // {
               //   offset: state.page * state.pageSize,
@@ -396,7 +409,7 @@ const GlygenTable = props => {
               // },
               true,
               null,
-              response => fetchSuccess(response, state),
+              (response) => fetchSuccess(response, state),
               fetchError
             );
           } else if (props.fetchWSCallFunction) {
@@ -406,8 +419,12 @@ const GlygenTable = props => {
             setRows(props.data.total);
             // setPages(Math.ceil(props.data.total / state.pageSize));
           } else {
-            setPageErrorMessage("GlygenTable must subscribe to one of these two props: ws or wsCallFunction");
-            console.error("GlygenTable must subscribe to one of these two props: ws or wsCallFunction");
+            setPageErrorMessage(
+              "GlygenTable must subscribe to one of these two props: ws or wsCallFunction"
+            );
+            console.error(
+              "GlygenTable must subscribe to one of these two props: ws or wsCallFunction"
+            );
           }
         }}
       />
@@ -450,7 +467,7 @@ const GlygenTable = props => {
   );
 
   function fetchSuccess(response, state) {
-    response.json().then(responseJson => {
+    response.json().then((responseJson) => {
       setData(responseJson.rows);
       setRows(responseJson.total);
       setPages(Math.ceil(responseJson.total / state.pageSize));
@@ -459,7 +476,7 @@ const GlygenTable = props => {
   }
 
   function fetchError(response) {
-    response.json().then(response => {
+    response.json().then((response) => {
       setPageErrorsJson(response);
     });
     setShowErrorSummary(true);
@@ -474,7 +491,7 @@ const GlygenTable = props => {
   }
 
   function deleteError(response) {
-    response.json().then(response => {
+    response.json().then((response) => {
       setPageErrorsJson(response);
     });
     setShowErrorSummary(true);
@@ -495,7 +512,7 @@ const GlygenTable = props => {
   }
 
   function isMakePublicFailure(response) {
-    response.json().then(responseJson => {
+    response.json().then((responseJson) => {
       setPageErrorsJson(responseJson);
     });
     setShowErrorSummary(true);
@@ -546,12 +563,12 @@ GlygenTable.propTypes = {
   deleteOnClick: PropTypes.func,
   customCommentColumn: PropTypes.bool,
   showViewIcon: PropTypes.bool,
-  viewUrl: PropTypes.string
+  viewUrl: PropTypes.string,
 };
 
 GlygenTable.defaultProps = {
   defaultSortColumn: "dateModified",
-  defaultSortOrder: 0
+  defaultSortOrder: 0,
 };
 
 export { GlygenTable };
