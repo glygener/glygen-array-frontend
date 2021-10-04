f1fc2a24ae9cffbe31b53a224b0f1788a1802322
Sandeep N <SN34351@uga.com>
2021-10-04 12:40:18 +0530

    updating react icons version to try resolving build error
    Signed-off-by: Sandeep N <SN34351@uga.com>

--- src/utils/wsUtils.js
+++ src/utils/wsUtils.js
@@ -355,21 +355,12 @@ export function getWsUrl(ws) {
  * @param {Function} errorFunction Callback to invoke on error
  * @param {Object} headers Headers object. If not passed, will set both Accept and Content-Type headers to application/json
  */
-export async function wsCall(
-  ws,
-  httpMethod,
-  wsParams,
-  useToken,
-  body,
-  successFunction,
-  errorFunction,
-  headers
-) {
+export async function wsCall(ws, httpMethod, wsParams, useToken, body, successFunction, errorFunction, headers) {
   var url = getWsUrl(ws) + getWsParamString(wsParams);
   if (!headers) {
     headers = {
       Accept: "application/json",
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     };
   }
 
@@ -390,14 +381,14 @@ export async function wsCall(
           // mode: "no-cors", // 'cors' by default
           method: httpMethod,
           headers: headers,
-          body: body,
+          body: body
         })
       : await trackPromise(
           fetch(url, {
             // mode: "no-cors", // 'cors' by default
             method: httpMethod,
             headers: headers,
-            body: body,
+            body: body
           })
         );
 
@@ -434,7 +425,7 @@ function getWsParamString(wsParams) {
 
 function getUrlString(params) {
   var paramString = "";
-  params.forEach(function (val) {
+  params.forEach(function(val) {
     paramString += "/" + val;
   });
   return paramString;
@@ -443,7 +434,7 @@ function getUrlString(params) {
 function getQueryString(params) {
   var paramString = "";
   var firstParam = true;
-  Object.keys(params).forEach(function (key) {
+  Object.keys(params).forEach(function(key) {
     if (firstParam) {
       paramString += "?" + key + "=" + params[key];
       firstParam = false;
